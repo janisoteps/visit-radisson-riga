f9d4931eded8c5aa10e84cf2c229c9f8bc64e719
janisoteps <janis.oteps@gmail.com>
2022-08-10 21:49:37 +0200

    initial commit

---
+++ app/routes/index.tsx
@@ -0,0 +1,9 @@
+export default function Index() {
+    return (
+        <div style={{fontFamily: "system-ui, sans-serif", lineHeight: "1.4"}}>
+            <h1>The beginnings of Visit Radisson Riga page</h1>
+
+
+        </div>
+    );
+}
