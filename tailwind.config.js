811ae52ce3bcd687979a7c649009fe228213d67f
janisoteps <janis.oteps@gmail.com>
2022-09-01 16:39:42 +0200

    feedback implementation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,17 @@
 module.exports = {
     content: ["./app/**/*.{js,ts,jsx,tsx}"],
     theme: {
+        colors: {
+            'radisson1': '#559cab',
+            'radisson2': '#5296a5',
+            'parkinn1': '#003068',
+            'parkinn2': '#013674',
+            'blu1': '#024ea4',
+            'blu2': '#014897',
+            'blue': {
+                '500': ''
+            }
+        },
         extend: {},
     },
     plugins: [],
